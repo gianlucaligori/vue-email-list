897091a1bdc581bf3738090bc5121926b5cb1ef2
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-05-16 17:32:32 +0200

    end, no bonus

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 const app = Vue.createApp({
 	data() {
 		return {
-			randomMail: 0,
+			randomMail: [],
 		}
 	},
 	methods: {
 		getRandomMail() {
-			axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-				.then(serverResponse => this.randomMail = serverResponse.data.response);
+
+            this.randomMail = [];
+
+
+            for (let i = 0; i < 10; i++) {
+                
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then(serverResponse => this.randomMail.push(serverResponse.data.response));
+            } 
 		},
+
+
+
+
+        
 	},
-	created() {
-		this.getRandomMail();
+	 created() {
+	 	this.getRandomMail();
 	},
 });
 
